f9d84411d44c8c52d244c32c0ff8a0fddec39bac
ctrl-esc <max@lsit.co>
2016-08-19 15:00:08 +0100

    Update honeywell_scanner_plugin.js

--- www/honeywell_scanner_plugin.js
+++ www/honeywell_scanner_plugin.js
@@ -4,7 +4,7 @@ var exec = require('cordova/exec');
 var HoneywellScannerPlugin = function() {
 
   this.scan = function(success_cb, error_cb){
-    exec(success_cb, error_cb, "BarcodeScannerPlugin", "scan", []);
+    exec(success_cb, error_cb, "BarcodeScannerPlugin", "scan", ["SYM_ALL"]);
   };
 
   this.startListen = function(success_cb, error_cb){
